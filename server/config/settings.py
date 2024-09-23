ab3f6d28cf1642f48cf72285f0451249c65ee4c6
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-09-23 10:04:37 +0900

    server:環境構築

--- server/config/settings.py
+++ server/config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-thj0=fgs^lk-mw-3m0b%+a5u4-k0#2n-e985u7=p4lmnwrwtv1'
+env=environ.Env()
+env.read_end('.env')
+
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -37,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    "rest_framework",
+    "debt"
 ]
 
 MIDDLEWARE = [
