b5b81289ecd7d071d3044960fe901f802dcae637
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-09-24 01:14:47 +0900

    ルーティング作成

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./login";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+);
