7c430cb27565d78e22b267efb2a114ed0860ea78
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-10-05 15:00:44 +0900

    日付整形修正

--- client/src/utils/dateFormatter.ts
+++ client/src/utils/dateFormatter.ts
@@ -1,3 +1,3 @@
 export const dateFormatter = (date: Date) => {
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
 };
