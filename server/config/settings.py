bdd8b82463b75b7c0cfc67559a2dd92651b9b2c7
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-10-28 01:09:55 +0900

    ディレクトリ名変更

--- server/config/settings.py
+++ server/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 env=environ.Env()
-env.read_end('.env')
+env.read_env('.env')
 
 SECRET_KEY = env("SECRET_KEY")
 
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'app.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
