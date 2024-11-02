89c7c27eab8f2114517ddb0a4291ab2a1a83b1b6
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-11-03 01:56:58 +0900

    UIとロジックを分離

--- client/src/page/dashboard/index.tsx
+++ client/src/page/dashboard/index.tsx
@@ -1,18 +1,8 @@
-import { Grid2 } from "@mui/material";
-import { Amount } from "../../components/amount";
-import { AmountTable, ListType } from "../../components/amountTable";
-import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { ListType } from "../../components/amountTable";
+import { useState } from "react";
+import { DashboardLayout } from "./layout";
 
 export const Dashboard = () => {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const code = queryParams.get("code");
-
-  const navigate = useNavigate();
-
-  const [userInfo, setUserInfo] = useState(null);
   const [debtList, setDebtList] = useState<ListType[] | null>([
     { name: "田中太郎", amount: 500 },
     { name: "田中太郎", amount: 500 },
@@ -23,64 +13,11 @@ export const Dashboard = () => {
   const [totalList, setTotalList] = useState<ListType[] | null>(null);
   const [creditList, setCreditList] = useState<ListType[] | null>(null);
 
-  const [isLogin, setIsLogin] = useState<boolean | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      console.log(code);
-      if (!code) return;
-      try {
-        const response = await axios.get(
-          `http://localhost:8000/api/user_data/`,
-          {
-            params: {
-              code,
-            },
-          }
-        );
-        setUserInfo(response.data);
-        setIsLogin(true);
-      } catch (error) {
-        console.log(error);
-        setIsLogin(false);
-      }
-    })();
-  }, [code]);
-
   return (
-    <Grid2 container rowSpacing={4} wrap="nowrap" direction="column">
-      <Grid2 size={12}>
-        <Amount isPlus={true} amount={100} />
-      </Grid2>
-      <Grid2 container columnSpacing={4}>
-        <Grid2 size={4}>
-          <AmountTable
-            title="Debt"
-            isPlus={false}
-            amount={100}
-            list={debtList}
-            detailLink=""
-          />
-        </Grid2>
-        <Grid2 size={4}>
-          <AmountTable
-            title="Total"
-            isPlus={false}
-            amount={100}
-            list={totalList}
-            detailLink=""
-          />
-        </Grid2>
-        <Grid2 size={4}>
-          <AmountTable
-            title="Credit"
-            isPlus={true}
-            amount={100}
-            list={creditList}
-            detailLink=""
-          />
-        </Grid2>
-      </Grid2>
-    </Grid2>
+    <DashboardLayout
+      debtList={debtList}
+      totalList={totalList}
+      creditList={creditList}
+    />
   );
 };
