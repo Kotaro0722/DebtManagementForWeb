37ecbe7b49962949e8f8bceed402a45e6941c1db
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-11-03 01:19:40 +0900

    クロスオリジン設定

--- client/src/page/dashboard/index.tsx
+++ client/src/page/dashboard/index.tsx
@@ -2,8 +2,17 @@ import { Grid2 } from "@mui/material";
 import { Amount } from "../../components/amount";
 import { AmountTable, ListType } from "../../components/amountTable";
 import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const Dashboard = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const code = queryParams.get("code");
+
+  const navigate = useNavigate();
+
+  const [userInfo, setUserInfo] = useState(null);
   const [debtList, setDebtList] = useState<ListType[] | null>([
     { name: "田中太郎", amount: 500 },
     { name: "田中太郎", amount: 500 },
@@ -14,11 +23,29 @@ export const Dashboard = () => {
   const [totalList, setTotalList] = useState<ListType[] | null>(null);
   const [creditList, setCreditList] = useState<ListType[] | null>(null);
 
-  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [isLogin, setIsLogin] = useState<boolean | null>(null);
 
   useEffect(() => {
-    (async () => {})();
-  }, []);
+    (async () => {
+      console.log(code);
+      if (!code) return;
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/api/user_data/`,
+          {
+            params: {
+              code,
+            },
+          }
+        );
+        setUserInfo(response.data);
+        setIsLogin(true);
+      } catch (error) {
+        console.log(error);
+        setIsLogin(false);
+      }
+    })();
+  }, [code]);
 
   return (
     <Grid2 container rowSpacing={4} wrap="nowrap" direction="column">
--- server/config/settings.py
+++ server/config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     "rest_framework",
     "debt"
 ]
@@ -54,9 +55,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
