65f221759de1defd6e2d8443b7c6841d54c46fef
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-11-12 01:04:45 +0900

    server : エラー解消

--- server/debt/urls.py
+++ server/debt/urls.py
@@ -1,10 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import DiscordLoginView, MemberViewSet
+from .views import DiscordLoginView
 
 router = routers.DefaultRouter()
-router.register(r"member", MemberViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
